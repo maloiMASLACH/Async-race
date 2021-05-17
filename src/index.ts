ba35c06879e8708c001c48591fba8cabef2244c5
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-05-17 19:46:21 +0300

    docs: started work with progect

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world');
+import './style.css';
+import { App } from './app';
+
+window.onload = () => {
+  const appElement = document.getElementById('app');
+  if (!appElement) throw Error('error');
+  new App(document.body);
+};
