7093df7b758cec2cf86cc6735a1bf634e895166e
maloiMASLACH <60549661+maloiMASLACH@users.noreply.github.com>
2021-06-16 23:22:25 +0300

    Revert "Async-race"

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,10 @@
-import { Header, PageID } from './components/header/header';
-import { Garage } from './components/garage/garage';
-import { Winners } from './components/winners/winners';
-import { Page } from './components/templates/page';
+import { CardsField } from './components/cards-field/cards-field';
 
 export class App {
-  static contaiter: HTMLElement = document.body;
+  private readonly cardsField: CardsField;
 
-  private static defaultPage = 'current';
-
-  private header:Header;
-
-  static renderPage(idPage:string) {
-    const currentPage = document.querySelector(`#${App.defaultPage}`);
-    if (currentPage) {
-      currentPage.remove();
-    }
-    let page: Page | null = null;
-
-    if (idPage === PageID.GarageP) {
-      page = new Garage(idPage);
-    } else if (idPage === PageID.WinnersP) {
-      page = new Winners(idPage);
-    }
-
-    if (page) {
-      const pageHTML = page.render();
-      pageHTML.id = App.defaultPage;
-      App.contaiter.append(pageHTML);
-    }
+  constructor(private readonly rootElement: HTMLElement) {
+    this.cardsField = new CardsField();
+    this.rootElement.appendChild(this.cardsField.element);
   }
-
-  private enableChange = () => {
-    window.addEventListener('hashchange', () => {
-      const hash = window.location.hash.slice(1);
-      App.renderPage(hash);
-    });
-  };
-
-  constructor() {
-    this.header = new Header('header', 'head-menu');
-  }
-
-  start = () => {
-    App.contaiter.append(this.header.render());
-    App.renderPage('garage');
-    this.enableChange();
-    localStorage.setItem('page', '1');
-    localStorage.setItem('pageW', '1');
-    sessionStorage.setItem('nameAddID', '');
-    sessionStorage.setItem('colorAddID', '');
-    sessionStorage.setItem('nameUpdateID', '');
-    sessionStorage.setItem('colorUpdateID', '');
-  };
 }
